46dfc865fc69c5c070ed1f765c2109341f8f4ace
benjamin kunkel <benjamink1125@gmail.com>
2022-11-15 15:09:55 -0800

    initial commit

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace FizzBuzz;
+class program
+{
+    static void main(string[] args)
+    {
+        for (int i = 0; i < 100; i++)
+        {
+            Console.WriteLine($"{i}: {FizzBuzz(i)}");
+        }
+    }
+    public static string FizzBuzz(int num)
+    {
+        var answer = "";
+
+        if (num % 3 == 0)
+        {
+            answer += "Fizz";
+        }
+        if (num % 5 == 0)
+        {
+            answer += "Buzz";
+        }
+
+        return answer;
+
+
+
+    }
+}
+
+
+
+
+
